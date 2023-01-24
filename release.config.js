b555a48885d9bbc00b1d67dd8724e03857c9e238
Danilo Pianini <danilo.pianini@gmail.com>
2023-01-24 16:16:21 +0100

    Initial commit

---
+++ release.config.js
@@ -0,0 +1,25 @@
+var prepareCmd = `
+echo version=\${nextRelease.version} > gradle.properties
+echo VERSION="\${nextRelease.version}" > .env
+echo PROJECT_NAME=$(grep -Po 'rootProject\\s*\\.\\s*name\\s*=\\s*"\\K[\\w-]+(?=")' settings.gradle.kts) >> .env
+git commit -a -m "chore(release): \${nextRelease.version} [skip ci]"
+git push
+docker compose build
+`
+var publishCmd = `
+docker compose push
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "prepareCmd": prepareCmd,
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [ 
+            { "path": "charts.tar.zst" },
+         ]
+    }],
+    "@semantic-release/git",
+)
+module.exports = config
