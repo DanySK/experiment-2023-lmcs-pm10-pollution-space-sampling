43bcd5bd6c8081d423f04837c15240f669156cda
Danilo Pianini <danilo.pianini@unibo.it>
2023-01-26 18:10:08 +0100

    fix(compose) declare dependency between preparation and download

--- scripts/download-from-zenodo.sh
+++ scripts/download-from-zenodo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-RESPONSE="$(curl -sL -H "Accept: application/json" https://zenodo.org/api/records/7546592)"
+RESPONSE="$(curl -sL -H 'Accept: application/json' 'https://zenodo.org/api/records/7546592')"
+echo "Received response: $RESPONSE"
 LINK_AND_MD5="$(
   echo "$RESPONSE" | jq '
     .files
@@ -8,11 +9,13 @@ LINK_AND_MD5="$(
     | { name: .filename, link: .links.download, checksum: .checksum }
   '
 )"
+echo "Filtered response: $LINK_AND_MD5"
 FILE="/output/$(echo "$LINK_AND_MD5" | jq -r '.name')"
 MD5="$(echo "$LINK_AND_MD5" | jq -r '.checksum'))"
 if [ -s "$FILE" ] && [ "$(md5sum "$FILE" | awk '{print $1}')" = "$MD5" ]; then
   echo "File $FILE already exists and checksum matches $MD5, skipping download"
 else
-  echo "Downloading $FILE"
-  curl -sL "$(echo "$LINK_AND_MD5" | jq -r '.link')" -o "/output/$FILE"
+  URL="$(echo "$LINK_AND_MD5" | jq -r '.link')"
+  echo "Downloading $FILE from $URL"
+  curl -sL "$URL" -o "$FILE"
 fi
